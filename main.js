c48b86f3373ac16a4cbdc4d8596b5f351c96a030
Anders Eriksson <aeri3@kth.se>
2019-02-19 00:54:56 +0100

    marker only moves when mouse down

--- main.js
+++ main.js
@@ -5,6 +5,26 @@ class Vector2 {
     }
 }
 
+class Marker {
+    constructor(x, y) {
+        this.pos = new Vector2(x, y);
+        this.img = new Image();
+        this.img.src = "res/marker.png";
+        this.w = this.img.width;
+        this.h = this.img.height;
+    }
+
+    update(dt) {
+        if (mouseDown) {
+            this.pos = new Vector2(mouse.x - this.w / 2, mouse.y - this.h / 2);
+        }
+    }
+
+    render(ctx) {
+        ctx.drawImage(this.img, this.pos.x, this.pos.y);
+    }
+}
+
 function main() {
     var canvas = document.getElementById("ouijaCanvas");
     init(canvas);
@@ -18,7 +38,12 @@ function init(canvas) {
     canvas.width = innerWidth;
     canvas.height = innerHeight;
     canvas.addEventListener("mousemove", setMousePosition, false);
+    canvas.addEventListener("mousedown", mouseDown, false);
+    canvas.addEventListener("mouseup", mouseUp, false);
+    //canvas.addEventListener("mouseleave", mouseUp, false);
     window.mouse = new Vector2(0, 0);
+    window.mouseDown = false;
+    window.marker = new Marker(0, 0);
     window.canvasPos = getElementPosition(canvas);
 }
 
@@ -32,19 +57,25 @@ function gameLoop(ts, prevTs, canvas, ctx) {
 }
 
 function update(dt) {
-
+    marker.update(dt);
 }
 
 function render(canvas, ctx) {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    var marker = new Image();
-    marker.src = "res/marker.png";
-    ctx.drawImage(marker, window.mouse.x - marker.width / 2, window.mouse.y - marker.height / 2);
+    marker.render(ctx);
 }
 
 function setMousePosition(e) {
     window.mouse = new Vector2(e.clientX - canvasPos.x, e.clientY - canvasPos.y);
+}
+
+function mouseDown(e) {
+    window.mouseDown = true;
+}
+
+function mouseUp(e) {
+    window.mouseDown = false;
 }
 
 function getElementPosition(el) {
