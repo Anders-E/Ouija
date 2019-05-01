60537b4ce95fc948e4223c956640ef8765ada4ff
Anders Eriksson <aeri3@kth.se>
2019-05-01 20:34:28 +0200

    add missing accessibility modifiers to Board

--- src/server/board.ts
+++ src/server/board.ts
@@ -1,10 +1,8 @@
-import { Vector2 } from "./vector2";
-
 export class Board {
-    w: number;
-    h: number;
+    public w: number;
+    public h: number;
 
-    constructor(w: number, h: number) {
+    public constructor(w: number, h: number) {
         this.w = w;
         this.h = h;
     }
