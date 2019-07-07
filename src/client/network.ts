3869e86d0f35d3f0b035bb52d42361114ee7e037
Anders Eriksson <aeri3@kth.se>
2019-07-08 00:32:42 +0200

    minor refactor in network.ts

--- src/client/network.ts
+++ src/client/network.ts
@@ -8,13 +8,10 @@ export class Network {
 
     public constructor() {
         this.socket = io();
-        console.log(this.socket);
         this.socket.on('connect', (): void => {
-            this.socket.on('game_marker_pos', (position: MarkerPositionMsg): void => {
-                this.markerPosition = position;
-            });
-            this.socket.on('playerJoined', this.onPlayerJoined);
-            this.socket.on('playerLeft', this.onPlayerLeft);
+            this.socket.on('game_marker_pos', this.onGameMarkerPosition.bind(this));
+            this.socket.on('playerJoined', this.onPlayerJoined.bind(this));
+            this.socket.on('playerLeft', this.onPlayerLeft.bind(this));
 
             console.log('connected to server');
             console.log(this.socket);
@@ -31,6 +28,10 @@ export class Network {
 
     public getSocket(): SocketIOClient.Socket {
         return this.socket;
+    }
+
+    private onGameMarkerPosition(position: MarkerPositionMsg): void {
+        this.markerPosition = position;
     }
 
     private onPlayerJoined(id: string): void {
